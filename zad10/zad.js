b4151e74dc54094abc33a13c0055d0b426df1504
Mariusz.C <147604943+SiekieraThc06@users.noreply.github.com>
2025-03-06 20:28:16 +0100

    Delete zad10/zad.js

--- zad10/zad.js
+++
@@ -1,9 +0,0 @@
-function dodanie(){
- 
-    var tekst = document.getElementById('pole1').textContent;
-    var newElement = document.createElement('li');
-    var newText = document.createTextNode(tekst);
-    newElement.appendChild(newText);
-    var position = document.getElementsByTagName('ul')[0];
-    position.appendChild(newElement);
-    }
